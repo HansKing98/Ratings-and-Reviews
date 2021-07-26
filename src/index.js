533954cace390c3f38cc644bd92834536430da84
hans <im.hansking@gmail.com>
2021-07-26 14:03:57 +0800

    init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import Reviews from "../pages/reviews";
+import Reviews from "./pages/reviews";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
